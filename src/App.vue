b58385eaf4648af827d1781f3b6d04d297e12750
SELVAPRAVEEN-05 <selvapraveen2005@gmail.com>
2025-07-15 22:33:16 +0530

    first

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <ImageGallery />
+  </div>
+</template>
+
+<script>
+import ImageGallery from './components/imageGallary.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ImageGallery,
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
